cfd6a6c011c39055adbcbd2040dfb60c61b2df63
Eric-HAN-01 <hhw3422@gmail.com>
2026-03-28 15:08:57 +0900

    chore: 프로젝트세팅

---
+++ src/main/java/com/_s3k/runsync/RunsyncApplication.java
@@ -0,0 +1,13 @@
+package com._s3k.runsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunsyncApplication.class, args);
+	}
+
+}
