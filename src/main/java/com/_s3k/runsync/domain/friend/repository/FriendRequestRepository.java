7a1860db029db37ad964de287c4afaa7156ef3c2
feature/user-info-api <ktk010112@gmail.com>
2026-06-06 19:28:11 +0900

    refactor: 코드 리뷰 반영 (N+1 방지 @EntityGraph 추가)

--- src/main/java/com/_s3k/runsync/domain/friend/repository/FriendRequestRepository.java
+++ src/main/java/com/_s3k/runsync/domain/friend/repository/FriendRequestRepository.java
@@ -2,11 +2,14 @@ package com._s3k.runsync.domain.friend.repository;
 
 import com._s3k.runsync.entity.FriendRequest;
 import com._s3k.runsync.entity.enums.FriendRequestStatus;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
     boolean existsBySender_IdAndReceiver_IdAndStatus(Long senderId, Long receiverId, FriendRequestStatus status);
+
+    @EntityGraph(attributePaths = {"sender"})
     List<FriendRequest> findByReceiver_IdAndStatus(Long receiverId, FriendRequestStatus status);
 }
